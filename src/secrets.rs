1bbcefd86dae7256d0754b20b44337d29550fbd3
Mohamed Zenadi <mohamed@zenadi.com>
2020-06-23 20:58:27 +0200

    accept custom profile & region in the cli

--- src/secrets.rs
+++ src/secrets.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::str::FromStr;
+
+use anyhow::Result;
 use rusoto_core;
 use rusoto_secretsmanager::SecretsManagerClient;
 
@@ -10,9 +14,23 @@ pub struct Manager {
 }
 
 impl Manager {
-    pub fn new() -> Manager {
-        Manager {
-            client: SecretsManagerClient::new(rusoto_core::Region::default()),
+    /// Create a new manager client. Overrides the default `profile` and `region`
+    /// if they are provided.
+    pub fn new(profile: Option<String>, region: Option<String>) -> Result<Manager> {
+        // FIXME use the ProfileProvider::with_default_credentials(profile) once it's merged
+        //       in https://github.com/rusoto/rusoto/pull/1776
+        //       For the moment rely on a hackish env variable change
+        if let Some(profile) = profile {
+            env::set_var("AWS_PROFILE", profile);
         }
+
+        let region = match region {
+            Some(r) => rusoto_core::Region::from_str(&r)?,
+            None => rusoto_core::Region::default(),
+        };
+
+        Ok(Manager {
+            client: SecretsManagerClient::new(region),
+        })
     }
 }
